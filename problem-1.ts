a93ddc75ba2ee8f870b5e4fbb6e76bac157e2d11
Soumya Chimakurthy <chimakurthy.soumya@gmail.com>
2022-05-27 21:34:50 -0500

    Add comments

--- problem-1.ts
+++ problem-1.ts
@@ -1,3 +1,8 @@
+// Time Complexity : O(log n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+// Your code here along with comments explaining your approach
 class searchRange {
     result = [];
     nums = [5, 7, 7, 8, 8, 10];
@@ -8,18 +13,21 @@ class searchRange {
     }
 
     findStartPosition() {
-        let low = 0;
-        let high = this.nums.length - 1;
+        let low = 0; // first index
+        let high = this.nums.length - 1; // last index
 
         while (low <= high) {
+            // Calculate mid
             const mid = Math.floor(low + (high - low) / 2);
+            // if target is in the left side of the array reduce the high to mid - 1
             if (this.nums[mid] >= this.target) {
                 high = mid - 1;
             }
-
+            // if target is on right side then increase the low to mid + 1
             else {
                 low = mid + 1;
             }
+            // if mid matches target return the index
             if (this.nums[mid] === this.target) {
                  return mid;
             }
@@ -28,18 +36,21 @@ class searchRange {
     }
 
     findLastPosition() {
-        let low = 0;
-        let high = this.nums.length - 1;
+        let low = 0; // first index
+        let high = this.nums.length - 1; // last index
 
         while (low <= high) {
+            // calculate mid
             const mid = Math.floor(low + (high - low) / 2);
+            // if target is in the left side, increase low to mid + 1
             if (this.nums[mid] <= this.target) {
                 low = mid + 1;
             }
             else {
+                // if target is on the right side reduce high to mid - 1
                 high = mid - 1;
             }
-
+            // if mid matches target return the index 
             if (this.nums[mid] === this.target) {
                 return mid;
             }
